79218a821d82327597b75e61fb2741e03e450c5a
vedhamoorthy <vedha.316@gmail.com>
2023-11-14 16:57:50 +0530

    Added about.php

---
+++ about.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>About Page</title>
+  </head>
+  <body>
+    <div>
+      <a href="index.php">Home</a>
+      <a href="services.php">Services</a>
+    </div>
+    <div>
+      <h1>About Page</h1>
+    </div>
+  </body>
+</html>
