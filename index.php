d093064f64a937096779f08a14d82b6f8aaae0e3
vedhamoorthy <vedha.316@gmail.com>
2023-11-14 16:56:47 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <div>
+      <a href="about.php">About</a>
+      <a href="services.php">Services</a>
+    </div>
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  </body>
+</html>
